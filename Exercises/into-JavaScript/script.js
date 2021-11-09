444a07b2086b895072038f2cef7899b8b2f07ef8
ArnoBoom <90990663+ArnoBoom@users.noreply.github.com>
2021-11-09 01:10:20 -0500

    1

---
+++ Exercises/into-JavaScript/script.js
@@ -0,0 +1,16 @@
+let age=25;
+let height = 5;
+
+console.log (age);
+console.log (height );
+
+let noun="cat";
+let adj="white";
+let verb="jump";
+
+// console.log(noun );
+// console.log (adj);
+// console.log(verb);
+
+console.log(`My ${noun} leaps ${adj}, when I ${verb} to the sky`);
+
---
+++ Projects/Project3/p3script.js
@@ -0,0 +1,5 @@
+
+  window.scrollTo({
+    top: 200%;
+    behavior: 'smooth';
+  })
